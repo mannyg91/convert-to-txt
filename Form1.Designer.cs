038f93dcc56bac3193638c1e34354bcec79573b2
Manny Gomez <Manny.Gomez@lcb.state.nv.us>
2024-04-16 11:53:41 -0700

    updated default word count, minor styling adjustments, update to text

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -12,6 +12,7 @@ namespace CodeToTxt
 
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             chkHtml = new CheckBox();
             chkCss = new CheckBox();
             chkJs = new CheckBox();
@@ -40,7 +41,7 @@ namespace CodeToTxt
             chkHtml.AutoSize = true;
             chkHtml.Checked = true;
             chkHtml.CheckState = CheckState.Checked;
-            chkHtml.Location = new Point(316, 156);
+            chkHtml.Location = new Point(323, 199);
             chkHtml.Margin = new Padding(6, 7, 6, 7);
             chkHtml.Name = "chkHtml";
             chkHtml.Size = new Size(100, 36);
@@ -54,7 +55,7 @@ namespace CodeToTxt
             chkCss.AutoSize = true;
             chkCss.Checked = true;
             chkCss.CheckState = CheckState.Checked;
-            chkCss.Location = new Point(417, 156);
+            chkCss.Location = new Point(424, 199);
             chkCss.Margin = new Padding(6, 7, 6, 7);
             chkCss.Name = "chkCss";
             chkCss.Size = new Size(82, 36);
@@ -67,7 +68,7 @@ namespace CodeToTxt
             chkJs.AutoSize = true;
             chkJs.Checked = true;
             chkJs.CheckState = CheckState.Checked;
-            chkJs.Location = new Point(507, 156);
+            chkJs.Location = new Point(514, 199);
             chkJs.Margin = new Padding(6, 7, 6, 7);
             chkJs.Name = "chkJs";
             chkJs.Size = new Size(67, 36);
@@ -77,7 +78,7 @@ namespace CodeToTxt
             // 
             // btnBrowseFolder
             // 
-            btnBrowseFolder.Location = new Point(970, 232);
+            btnBrowseFolder.Location = new Point(977, 275);
             btnBrowseFolder.Margin = new Padding(6, 7, 6, 7);
             btnBrowseFolder.Name = "btnBrowseFolder";
             btnBrowseFolder.Size = new Size(162, 57);
@@ -88,7 +89,7 @@ namespace CodeToTxt
             // 
             // txtFolderPath
             // 
-            txtFolderPath.Location = new Point(313, 241);
+            txtFolderPath.Location = new Point(320, 284);
             txtFolderPath.Margin = new Padding(6, 7, 6, 7);
             txtFolderPath.Name = "txtFolderPath";
             txtFolderPath.ReadOnly = true;
@@ -97,7 +98,7 @@ namespace CodeToTxt
             // 
             // btnBrowseOutput
             // 
-            btnBrowseOutput.Location = new Point(970, 303);
+            btnBrowseOutput.Location = new Point(977, 346);
             btnBrowseOutput.Margin = new Padding(6, 7, 6, 7);
             btnBrowseOutput.Name = "btnBrowseOutput";
             btnBrowseOutput.Size = new Size(162, 57);
@@ -108,7 +109,7 @@ namespace CodeToTxt
             // 
             // txtOutputPath
             // 
-            txtOutputPath.Location = new Point(313, 312);
+            txtOutputPath.Location = new Point(320, 355);
             txtOutputPath.Margin = new Padding(6, 7, 6, 7);
             txtOutputPath.Name = "txtOutputPath";
             txtOutputPath.ReadOnly = true;
@@ -117,20 +118,20 @@ namespace CodeToTxt
             // 
             // nudMaxWords
             // 
-            nudMaxWords.Location = new Point(583, 502);
+            nudMaxWords.Location = new Point(590, 545);
             nudMaxWords.Margin = new Padding(6, 7, 6, 7);
-            nudMaxWords.Maximum = new decimal(new int[] { 10000, 0, 0, 0 });
+            nudMaxWords.Maximum = new decimal(new int[] { 1000000, 0, 0, 0 });
             nudMaxWords.Minimum = new decimal(new int[] { 1, 0, 0, 0 });
             nudMaxWords.Name = "nudMaxWords";
             nudMaxWords.Size = new Size(181, 39);
             nudMaxWords.TabIndex = 7;
-            nudMaxWords.Value = new decimal(new int[] { 1000, 0, 0, 0 });
+            nudMaxWords.Value = new decimal(new int[] { 50000, 0, 0, 0 });
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label1.Location = new Point(373, 502);
+            label1.Location = new Point(380, 545);
             label1.Margin = new Padding(6, 0, 6, 0);
             label1.Name = "label1";
             label1.Size = new Size(201, 32);
@@ -141,12 +142,12 @@ namespace CodeToTxt
             // 
             btnScan.BackColor = SystemColors.Highlight;
             btnScan.ForeColor = SystemColors.ButtonHighlight;
-            btnScan.Location = new Point(831, 490);
+            btnScan.Location = new Point(838, 533);
             btnScan.Margin = new Padding(6, 7, 6, 7);
             btnScan.Name = "btnScan";
             btnScan.Size = new Size(227, 57);
             btnScan.TabIndex = 9;
-            btnScan.Text = "Scan";
+            btnScan.Text = "Scan / Generate";
             btnScan.UseVisualStyleBackColor = false;
             btnScan.Click += btnScan_Click;
             // 
@@ -155,11 +156,11 @@ namespace CodeToTxt
             label2.AutoSize = true;
             label2.FlatStyle = FlatStyle.System;
             label2.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label2.Location = new Point(175, 156);
+            label2.Location = new Point(99, 199);
             label2.Name = "label2";
-            label2.Size = new Size(132, 32);
+            label2.Size = new Size(211, 32);
             label2.TabIndex = 10;
-            label2.Text = "File Types:";
+            label2.Text = "Target File Types:";
             // 
             // label3
             // 
@@ -167,24 +168,25 @@ namespace CodeToTxt
             label3.Font = new Font("Segoe UI", 14F, FontStyle.Bold | FontStyle.Underline, GraphicsUnit.Point);
             label3.Location = new Point(36, 34);
             label3.Name = "label3";
-            label3.Size = new Size(237, 51);
+            label3.Size = new Size(266, 51);
             label3.TabIndex = 11;
-            label3.Text = "Code-to-Txt";
+            label3.Text = "Code-to-TXTs";
             // 
             // label4
             // 
             label4.Font = new Font("Segoe UI", 8.5F, FontStyle.Regular, GraphicsUnit.Point);
-            label4.Location = new Point(307, 49);
+            label4.Location = new Point(320, 34);
             label4.Name = "label4";
-            label4.Size = new Size(825, 96);
+            label4.Size = new Size(891, 155);
             label4.TabIndex = 12;
-            label4.Text = "Scans project files and combines code onto one or more .txt files split up by the desired amount of characters. Useful for code analysis when using LLM models.";
+            label4.Text = resources.GetString("label4.Text");
+            label4.Click += label4_Click;
             // 
             // label5
             // 
             label5.AutoSize = true;
             label5.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label5.Location = new Point(128, 244);
+            label5.Location = new Point(135, 287);
             label5.Name = "label5";
             label5.Size = new Size(179, 32);
             label5.TabIndex = 13;
@@ -194,7 +196,7 @@ namespace CodeToTxt
             // 
             label6.AutoSize = true;
             label6.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label6.Location = new Point(71, 319);
+            label6.Location = new Point(78, 362);
             label6.Name = "label6";
             label6.Size = new Size(233, 32);
             label6.TabIndex = 14;
@@ -205,7 +207,7 @@ namespace CodeToTxt
             checkBox1.AutoSize = true;
             checkBox1.Checked = true;
             checkBox1.CheckState = CheckState.Checked;
-            checkBox1.Location = new Point(594, 156);
+            checkBox1.Location = new Point(601, 199);
             checkBox1.Margin = new Padding(6, 7, 6, 7);
             checkBox1.Name = "checkBox1";
             checkBox1.Size = new Size(72, 36);
@@ -218,7 +220,7 @@ namespace CodeToTxt
             checkBox2.AutoSize = true;
             checkBox2.Checked = true;
             checkBox2.CheckState = CheckState.Checked;
-            checkBox2.Location = new Point(685, 156);
+            checkBox2.Location = new Point(692, 199);
             checkBox2.Margin = new Padding(6, 7, 6, 7);
             checkBox2.Name = "checkBox2";
             checkBox2.Size = new Size(77, 36);
@@ -231,15 +233,16 @@ namespace CodeToTxt
             // 
             label7.AutoSize = true;
             label7.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point);
-            label7.Location = new Point(161, 388);
+            label7.Location = new Point(168, 431);
             label7.Name = "label7";
-            label7.Size = new Size(143, 32);
+            label7.Size = new Size(142, 32);
             label7.TabIndex = 19;
-            label7.Text = "Ignore File:";
+            label7.Text = "Ignore .txt:";
+            label7.Click += label7_Click;
             // 
             // txtIgnoreFilePath
             // 
-            txtIgnoreFilePath.Location = new Point(314, 383);
+            txtIgnoreFilePath.Location = new Point(321, 426);
             txtIgnoreFilePath.Margin = new Padding(6, 7, 6, 7);
             txtIgnoreFilePath.Name = "txtIgnoreFilePath";
             txtIgnoreFilePath.ReadOnly = true;
@@ -248,7 +251,7 @@ namespace CodeToTxt
             // 
             // btnBrowseIgnore
             // 
-            btnBrowseIgnore.Location = new Point(971, 374);
+            btnBrowseIgnore.Location = new Point(978, 417);
             btnBrowseIgnore.Margin = new Padding(6, 7, 6, 7);
             btnBrowseIgnore.Name = "btnBrowseIgnore";
             btnBrowseIgnore.Size = new Size(162, 57);
@@ -261,7 +264,7 @@ namespace CodeToTxt
             // 
             AutoScaleDimensions = new SizeF(13F, 32F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1180, 587);
+            ClientSize = new Size(1306, 642);
             Controls.Add(label7);
             Controls.Add(txtIgnoreFilePath);
             Controls.Add(btnBrowseIgnore);
--- Form1.cs
+++ Form1.cs
@@ -94,5 +94,15 @@ namespace CodeToTxt
                 }
             }
         }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
